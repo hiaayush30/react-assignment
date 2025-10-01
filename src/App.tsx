fed942c40fbdbf069a3028cc3210f5713e917e05
aayush <hiaayush30@gmail.com>
2025-10-01 17:43:27 +0530

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { useState, useEffect,type BaseSyntheticEvent } from 'react';
-import { DataTable } from 'primereact/datatable';
+import { useState, useEffect } from 'react';
+import { DataTable, type DataTablePageEvent } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 
 type ProductType = {
@@ -17,21 +17,41 @@ export default function BasicDemo() {
 
   useEffect(() => {
     const fetchData = async () => {
-      const response = await fetch("https://api.artic.edu/api/v1/artworks?page=1");
-      const data = await response.json();
-      setProducts(data.data)
+      try {
+        const response = await fetch("https://api.artic.edu/api/v1/artworks?page=1");
+        const data = await response.json();
+        setProducts(data.data)
+      } catch (error) {
+        console.log(error)
+      }
     }
     fetchData()
   }, []);
 
+  const fetchPage = async (page: number): Promise<void> => {
+    try {
+      const response = await fetch(`https://api.artic.edu/api/v1/artworks?page=${page}`);
+      const data = await response.json();
+      setProducts(data.data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  const handlePageChange = (event: DataTablePageEvent) => {
+    if (event.page) {
+      const newPage = event.page + 1;  
+      fetchPage(newPage);
+    }
+  };
+
   return (
     <div className="p-5 h-screen flex justify-center items-center">
       <DataTable
-        onClick={(e: BaseSyntheticEvent) => console.log(e)}
         value={products}
         tableStyle={{ minWidth: '50rem' }}
         paginator rows={5}
         rowsPerPageOptions={[5, 10, 25, 50]}
+        onPage={handlePageChange}
       >
         <Column field="title" header="Title"></Column>
         <Column field="place_of_origin" header="Place of Origin"></Column>
