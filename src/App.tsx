cd865425a4460053faa75cc0e172e6dd012f0a19
aayush <hiaayush30@gmail.com>
2025-10-01 17:28:09 +0530

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,44 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect,type BaseSyntheticEvent } from 'react';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
-// import { ProductService } from './service/ProductService';
+
+type ProductType = {
+  id: number;
+  title: string;
+  place_of_origin: string;
+  artist_display: string;
+  inscriptions: string;
+  date_start: Date;
+  date_end: Date
+}
 
 export default function BasicDemo() {
-  const [products, setProducts] = useState<any[]>([]);
+  const [products, setProducts] = useState<ProductType[]>([]);
 
   useEffect(() => {
-    // Generate random dataset
-    const mockProducts = Array.from({ length: 10 }, (_, i) => ({
-      code: "P" + (1000 + i),
-      name: ["Laptop", "Headphones", "Mouse", "Keyboard", "Monitor", "Phone", "Tablet", "Camera", "Speaker", "Printer"][
-        Math.floor(Math.random() * 10)
-      ],
-      category: ["Electronics", "Accessories", "Office", "Gaming", "Audio"][Math.floor(Math.random() * 5)],
-      quantity: Math.floor(Math.random() * 100) + 1,
-    }));
-
-    setProducts(mockProducts);
+    const fetchData = async () => {
+      const response = await fetch("https://api.artic.edu/api/v1/artworks?page=1");
+      const data = await response.json();
+      setProducts(data.data)
+    }
+    fetchData()
   }, []);
 
   return (
-    <div className="card">
-      <DataTable value={products} tableStyle={{ minWidth: '50rem' }} paginator rows={5} rowsPerPageOptions={[5, 10, 25, 50]}>
-        <Column field="code" header="Code"></Column>
-        <Column field="name" header="Name"></Column>
-        <Column field="category" header="Category"></Column>
-        <Column field="quantity" header="Quantity"></Column>
+    <div className="p-5 h-screen flex justify-center items-center">
+      <DataTable
+        onClick={(e: BaseSyntheticEvent) => console.log(e)}
+        value={products}
+        tableStyle={{ minWidth: '50rem' }}
+        paginator rows={5}
+        rowsPerPageOptions={[5, 10, 25, 50]}
+      >
+        <Column field="title" header="Title"></Column>
+        <Column field="place_of_origin" header="Place of Origin"></Column>
+        <Column field="artist_display" header="Artist"></Column>
+        <Column field="inscriptions" header="Inscriptions"></Column>
+        <Column field="date_start" header="Start Date"></Column>
+        <Column field="date_end" header="End Date"></Column>
       </DataTable>
     </div>
   );
