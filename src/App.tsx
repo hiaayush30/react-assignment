bb61fb10973b69cdbe55b747d34a3d52c09fa2cd
aayush <hiaayush30@gmail.com>
2025-10-01 16:49:17 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Button } from 'primereact/button';
+
+export default function BasicDemo() {
+    return (
+        <div className="card flex justify-content-center">
+            <Button label="Check" icon="pi pi-check" />
+        </div>
+    )
+}
+        
